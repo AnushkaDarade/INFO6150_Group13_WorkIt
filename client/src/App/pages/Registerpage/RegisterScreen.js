0d13982b8132672fe83429438b5c9b9c55c52304
Anushka Darade <darade.a@northeastern.edu>
2022-12-03 18:41:03 -0500

    Registration done

--- client/src/App/pages/Registerpage/RegisterScreen.js
+++ client/src/App/pages/Registerpage/RegisterScreen.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useState } from "react";
+import "./RegisterScreen.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
 function RegisterScreen() {
   const [name, setName] = useState("");
@@ -10,16 +14,20 @@ function RegisterScreen() {
   let handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      let res = await fetch("http://localhost:9002/users/create", {
+      let res = await fetch("http://localhost:9002/user/create", {
         method: "POST",
         body: JSON.stringify({
           name: name,
           email: email,
           password: password,
         }),
+        headers: { 'Content-Type': 'application/json' }
+
       });
+
+
       let resJson = await res.json();
-      if (res.status === 200) {
+      if (res.status === 201) {
         setName("");
         setEmail("");
         setPassword("");
@@ -32,11 +40,48 @@ function RegisterScreen() {
     }
   };
 
-
-
   return (
-    <div className="Register">
-      <form onSubmit={handleSubmit}>
+    <div className="App1">
+          
+          <Card>
+        <Card.Header className="register" as="h1">REGISTER</Card.Header>
+        <Card.Body>
+        <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label  className="name">Name</Form.Label>
+        <Form.Control className="inputs" type="text" 
+        value={name}
+        placeholder="Enter Name"
+        onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label  className="email">Email address</Form.Label>
+        <Form.Control className="inputs" type="email" 
+        value={email}
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label  className="password">Password</Form.Label>
+        <Form.Control className="inputs" type="password" 
+        value={password}
+        placeholder="Password" 
+        onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Create User
+      </Button>
+    </Form>
+        </Card.Body>
+      </Card>
+      <div className="message">{message ? <p>{message}</p> : null}</div>
+
+      {/* <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={name}
@@ -58,7 +103,7 @@ function RegisterScreen() {
 
         <button type="submit">Create</button>
         <div className="message">{message ? <p>{message}</p> : null}</div>
-      </form>
+      </form> */}
     </div>
   );
 }
