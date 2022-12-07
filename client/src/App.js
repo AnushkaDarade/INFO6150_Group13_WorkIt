74065d422851a4207af1e7f9e9ad47c320d5e024
Anushka Darade <darade.a@northeastern.edu>
2022-12-07 00:38:18 -0500

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,82 @@
-import './App.css';
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
-import Login from './App/pages/LoginPage/LoginPage'
-import Register from './App/pages/Registerpage/RegisterScreen'
-// import LandingPage from './App/pages/LandingPage'
-import  About from './App/pages/Aboutpage/AboutPage'
-import Contact from './App/pages/Contactpage/ContactPage'
+import React, {Component} from 'react';
+import {Route, HashRouter} from 'react-router-dom';
+import Home from './Components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Travel from './Components/Travel/Travel';
+import Food from './Components/Food';
+import Blog from './Components/Blog';
+import LoginModal from './Components/LoginModal';
+import SignUpModal from './Components/SignUpModal';
+import $ from 'jquery';
+import TopNav from './Components/TopNav';
+import BlogId from './Components/BlogId';
+import Footer from './Components/Footer/footer';
+import SocialMedia from './Components/SocialMedia.js/SocialMedia';
+import RealHome from './Components/RealHome';
+import About from './Components/About';
+import Contact from './Components/Contact/Contact'
 
-import Navbar from './App/components/Navbar'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-         <Navbar/>
+class App extends Component{
+	constructor(props){
+        super(props)
 
-         <Routes> 
-         <Route path='/' element={<Login/>}></Route>
-         <Route path='/register' element={<Register/>}></Route>
-         <Route path='/about' element={<About/>}></Route>
-         <Route path='/contact' element={<Contact/>}></Route>
+        this.state = {
+            credentials: {
+                "emailid":'',
+                "password":'',
+                "firstName":'',
+                "lastName":'',
+                "phoneNumber":''},
+            isLogged: false,
+            blogs: [],
+            loginShow: false,
+            signUpShow: false
+        }
+
         
-       </Routes>
-     </Router>
-    </div>
-  );
+    }
+
+	writeBlog = (details) => {
+		//blog should have tags, post, email
+	}
+
+    userAuth = () =>{
+        console.log(this.state)
+        var api="http://localhost:3000/user/getAll"
+
+        
+        
+        $.getJSON(api).then(function(data){
+            this.setState({users: data})
+            console.log(this.state)
+        }.bind(this))
+    }
+
+
+	render(){
+		return(
+            <HashRouter>
+				<TopNav/>
+                
+				<Route exact path ='/' component={RealHome}/>
+				<Route path ='/home' component={ RealHome} />
+				<Route path ='/travel' component={Travel} />
+				<Route path ='/food' component={Food} />
+                <Route path ='/about' component={About} />
+                <Route path ='/contact' component={Contact} />
+                <Route path ='/blog' component={Home} />
+				<Route path ='/myblogs' component={ (props) => <Blog credentials= {this.state.credentials} blogs={this.state.blogs} writeBlog = {(details)=>this.writeBlog(details)}/>} />				
+                <Route exact path="/signup" component={SignUpModal} />
+                <Route exact path="/login" component={LoginModal} />
+                <Route  path="/post/:id" component={BlogId} />
+             
+                <SocialMedia />
+          <Footer />
+          </HashRouter>
+		)
+	}
 }
 
 export default App;
