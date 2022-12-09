ea6eda5afb0f9002b350d74f25c45483aa8887ad
PrarthanaNemade14 <nemade.pr@northeastern.edu>
2022-12-08 19:49:04 -0500

    Footer- Done

--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -8,7 +8,7 @@ function footer() {
         <>
             
             <h5 className="text-center copyright"> 
-            <h5>WORK IT!</h5>
+            <h5><b>WORK IT!</b></h5>
             <h6>Get in touch with us here: <br></br> <a href="mailto:workit@gmail.com">workit@gmail.com</a> or  
             <a href="tel:+1 857 987 8987">+1 +857 987 8987</a></h6>
 
