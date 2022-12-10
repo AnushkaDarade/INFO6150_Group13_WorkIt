d7f8c292b70c01c6cbe090755c7a0a4fc7a53623
swethapaturu9 <paturu.s@northeastern.edugit config user.name paturu.s@northeastern.edu>
2022-12-09 23:52:30 -0500

    link todo ui

--- server/app/controllers/note.controller.js
+++ server/app/controllers/note.controller.js
@@ -11,12 +11,12 @@ const asyncHandler = require("express-async-handler");
 // });
 
 exports.getAllTodos = (req,res) => {
-Note.find().then(notes => {
-    console.log(notes)
-     if(notes){
+Note.find().then(documents => {
+    console.log(documents)
+     if(documents){
         res.status(200).json({
         message: "All Todos fetched successfully!",
-        todos: notes
+        todos: documents
 
      });
      } 
